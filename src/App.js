c10e088075ea5ae84971c242481778f93951bb09
ptg0811 <ptg0811@gmail.com>
2023-05-22 06:28:18 +0900

    try commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [value, setValue] = useState("");
+
+  const onChangeEventHandler = (event) => {
+    const value = event.target.value;
+    setValue(value);
+  };
+
+  console.log(value);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {value}
+      <input
+        type="text"
+        onChange={onChangeEventHandler}
+        placeholder="이름을 입력해주세요"
+      />
     </div>
   );
-}
+};
 
 export default App;
